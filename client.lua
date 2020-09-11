150465d9b4090e618a7af28dccd649a23ef8b471
Nikiteus <vilenikita@gmail.com>
2020-09-12 02:50:51 +0300

    Revert "Set "confirm" flag on client side to true"
    This reverts commit f1f8569d5140c83d156e6e102461ecc04be491bf.

--- client.lua
+++ client.lua
@@ -88,7 +88,7 @@ Citizen.CreateThread(function()
 							TriggerServerCallback('xnVending:checkMoneyandInvent', function(response) end, machine, i, true)
 							usingMachine = false
 						end
-					end, machine, i, true)
+					end, machine, i, false)
 				end
 			end
 			local scaleForm = setupScaleform("instructional_buttons", buttonsMessage)
